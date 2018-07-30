a3f5eea6bf7deea3af06d9ffb9925f70a6c04df7
LEI <guillaume.frichet@gmail.com>
2018-07-30 02:25:23 +0200

    +

--- install.sh
+++ install.sh
@@ -7,14 +7,18 @@ if hash yaourt 2>/dev/null; then
 fi
 
 _makepkg() {
-  # --syncdeps
-  makepkg --install --noprogressbar
+  makepkg --syncdeps --install --noprogressbar
 }
 
 TMPDIR=$(mktemp -d)
+
 cd "$TMPDIR"
+
 git clone --quiet https://aur.archlinux.org/package-query.git
+pacman -Syu --noconfirm yajl
 (cd package-query && _makepkg)
+
 git clone --quiet https://aur.archlinux.org/yaourt.git
 (cd yaourt && _makepkg)
+
 ls -la "$TMPDIR"
